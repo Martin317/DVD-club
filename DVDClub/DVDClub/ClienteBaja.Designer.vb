b53410b6c1cad6fc59fe25a3e7782b74aebc45c1
Franco <franco-cipo@hotmail.es>
2015-07-08 13:28:57 -0300

    ClienteBaja Form

--- DVDClub/DVDClub/ClienteBaja.Designer.vb
+++ DVDClub/DVDClub/ClienteBaja.Designer.vb
@@ -22,8 +22,64 @@ Partial Class ClienteBaja
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.labCliente = New System.Windows.Forms.Label()
+        Me.cmbCliente = New System.Windows.Forms.ComboBox()
+        Me.btnAtras = New System.Windows.Forms.Button()
+        Me.btnAceptar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'labCliente
+        '
+        Me.labCliente.AutoSize = True
+        Me.labCliente.Location = New System.Drawing.Point(49, 37)
+        Me.labCliente.Name = "labCliente"
+        Me.labCliente.Size = New System.Drawing.Size(39, 13)
+        Me.labCliente.TabIndex = 30
+        Me.labCliente.Text = "Cliente"
+        '
+        'cmbCliente
+        '
+        Me.cmbCliente.FormattingEnabled = True
+        Me.cmbCliente.Location = New System.Drawing.Point(94, 34)
+        Me.cmbCliente.Name = "cmbCliente"
+        Me.cmbCliente.Size = New System.Drawing.Size(126, 21)
+        Me.cmbCliente.TabIndex = 29
+        '
+        'btnAtras
+        '
+        Me.btnAtras.Location = New System.Drawing.Point(155, 74)
+        Me.btnAtras.Name = "btnAtras"
+        Me.btnAtras.Size = New System.Drawing.Size(75, 23)
+        Me.btnAtras.TabIndex = 31
+        Me.btnAtras.Text = "Atrás"
+        Me.btnAtras.UseVisualStyleBackColor = True
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Location = New System.Drawing.Point(74, 74)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 32
+        Me.btnAceptar.Text = "Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'ClienteBaja
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 113)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Me.btnAtras)
+        Me.Controls.Add(Me.labCliente)
+        Me.Controls.Add(Me.cmbCliente)
+        Me.Name = "ClienteBaja"
         Me.Text = "ClienteBaja"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents labCliente As System.Windows.Forms.Label
+    Friend WithEvents cmbCliente As System.Windows.Forms.ComboBox
+    Friend WithEvents btnAtras As System.Windows.Forms.Button
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
 End Class
