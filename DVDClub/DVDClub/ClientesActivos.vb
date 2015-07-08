e67908f88ec112b82d3a43cb48c809d2327a9462
Franco <franco-cipo@hotmail.es>
2015-07-08 12:05:57 -0300

    Formularios

---
+++ DVDClub/DVDClub/ClientesActivos.vb
@@ -0,0 +1,3 @@
+﻿Public Class ClientesActivos
+
+End Class
--- DVDClub/DVDClub/Form1.vb
+++ DVDClub/DVDClub/Form1.vb
@@ -1,19 +1,4 @@
 ﻿Public Class Form1
 
-    
-    Private Sub btnNuevaPelicula_Click(sender As Object, e As EventArgs) Handles btnNuevaPelicula.Click
-        PeliculaNueva.Show()
-    End Sub
-
-    Private Sub btnModificarPelicula_Click(sender As Object, e As EventArgs) Handles btnModificarPelicula.Click
-        PeliculaConsultar.Show()
-    End Sub
-
-    Private Sub btnConsultarPelicula_Click(sender As Object, e As EventArgs) Handles btnConsultarPelicula.Click
-        PeliculaModificar.Show()
-    End Sub
-
-    Private Sub btnBajaPelicula_Click(sender As Object, e As EventArgs) Handles btnBajaPelicula.Click
-        PeliculaBaja.Show()
-    End Sub
+ 
 End Class
