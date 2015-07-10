f95d1576c8ac8bf2c8c571a0b35beed89e1d3641
Martin317 <martinbrunori316@gmail.com>
2015-07-10 11:44:24 -0300

    Clase conexion
    agregue la clase conexion, hago el commit porque me tengo que í a comé

---
+++ DVDClub/DVDClub/Datos/Conexion.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Data.SqlClient
+Public Class Conexion
+    Protected cnn As New SqlConnection 'variable para establecer conexion a bd
+    Public idUsuario As Integer
+
+    Protected Function conectar()
+        Try
+            cnn = New SqlConnection("Data Source=MARTINPC\SQLExpress;User ID=sa;Password=qweasdzxc159753;Initial Catalog=dvdclubdb;Integrated Security=False")
+            cnn.Open()
+            Return True
+        Catch ex As Exception
+            MessageBox.Show("Atencion: Error al establecer la conexion a la base de datos" & Environment.NewLine & "Descripcion del error:" & Environment.NewLine & ex.Message,
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End Try
+    End Function
+
+    Protected Function desconectar()
+        Try
+            If cnn.State = ConnectionState.Open Then
+                cnn.Close()
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Atencion: Error al cerrar la conexion a la base de datos" & Environment.NewLine & "Descripcion del error:" & Environment.NewLine & ex.Message,
+                      "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End Try
+
+    End Function
+End Class
